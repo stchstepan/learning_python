252499c9f76e669040f47333970f645105c1f4cb
Stepan Cherkashin <120642009+stchstepan@users.noreply.github.com>
2023-07-12 18:41:49 +0300

    Create 5lesson.py

---
+++ list_manipulation/5lesson.py
@@ -0,0 +1,12 @@
+print("#1")
+magicans = ['alice', 'david', 'carolina']
+for magican in magicans:
+    print(magican)
+print("\n")
+
+print("#2")
+cars = ['lada', 'ferrari', 'volvo']
+for car in cars:
+    print(f"{car.title()}, beatiful car!")
+    print(f"I can't wait to see your next car, {car.title()}. \n")
+print("Thank you, everyone. That was a great car show!")
