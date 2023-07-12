73e191af38ccb147d2bd67ebbd69e0a495abd2a5
Stepan Cherkashin <120642009+stchstepan@users.noreply.github.com>
2023-07-12 18:49:04 +0300

    Add files via upload

---
+++ 4_list_manipulation/5lesson_practic.py
@@ -0,0 +1,13 @@
+print("#1")
+pizzas = ['hawaii', 'pepperoni', 'margarita', 'four cheese']
+for pizza in pizzas:
+  print(f"I like {pizza.title()} pizza!")
+print("I really like pizza")
+print("\n")
+
+print("#2")
+pets = ['cat', 'dog', 'bird', 'fish']
+for pet in pets:
+  print(f"A {pet} would make a great pet!")
+print("Any of these animals would make a great pet!")
+
